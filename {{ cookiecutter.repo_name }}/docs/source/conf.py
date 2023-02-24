72e78b32d4deeb8337d70d2345c7ae954030e40e
Maksim Rakitin <mrakitin@bnl.gov>
2023-02-24 14:55:13 -0500

    Dynamically calculate the year (for license/copyright)
    https://github.com/cookiecutter/cookiecutter/issues/902.

--- {{ cookiecutter.repo_name }}/docs/source/conf.py
+++ {{ cookiecutter.repo_name }}/docs/source/conf.py
@@ -68,7 +68,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "{{ cookiecutter.project_name }}"
-copyright = "{{ cookiecutter.year }}, {{ cookiecutter.full_name }}"
+copyright = {% now 'utc', '%Y' %}, {{ cookiecutter.full_name }}"
 author = "{{ cookiecutter.full_name }}"
 
 # The version info for the project you're documenting, acts as replacement for
